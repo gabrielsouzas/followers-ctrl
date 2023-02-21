8d5ae60eb11abc8599d00305ff76fbc1fb97d514
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-02-21 14:50:48 -0300

    Ajustes

--- github-api-fetch/fetchFollowers.js
+++ github-api-fetch/fetchFollowers.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const properties = require('./properties.json');
 
 const octokit = new Octokit();
-// aumentar paginas recebidas
-const fetchFollowers = async (value, user, filter = '') => {
-  const response = await octokit.request(`GET /users/{username}/${value}?per_page=100`, {
+// A FAZER: aumentar paginas recebidas
+const fetchFollowers = async (value, user, filter = '', per_page = '100') => {
+  const response = await octokit.request(`GET /users/{username}/${value}?per_page=${per_page}`, {
     username: user
   })
 
@@ -23,15 +23,34 @@ const fetchFollowers = async (value, user, filter = '') => {
     }))
   */
 
-  // escrever os dados em um arquivo local (json)
-  fs.writeFile(`${value}.json`, JSON.stringify(data, null, 2), err => {
-    if(err) throw new Error('Erro: '+ err)
-
-    console.log(`${value} fetched successfully!`)
-  })
-
-  //return data;
+  return data;
 };
 
-fetchFollowers('followers', properties.login);
-fetchFollowers('following', properties.login);
+// escrever os dados em um arquivo local (json)
+function writeJsonFile(fileName, fileData){
+  fs.writeFile(`${fileName}.json`, JSON.stringify(fileData, null, 2), err => {
+    if(err) throw new Error('Erro: '+ err)
+
+    console.log(`${fileName} fetched successfully!`)
+  })
+}
+
+async function verifyUsersFollowing(){
+  var arrayRobots = [];
+  let followingLogins = await fetchFollowers('following', properties.login);
+  followingLogins.forEach(async el => {
+    let userFollowing = await fetchFollowers('following', el.login, 'login', '5');
+    console.log(el.login)
+    console.log(userFollowing.length)
+  });
+}
+
+(async () => {
+  const followers = await fetchFollowers('followers', properties.login);
+  const following = await fetchFollowers('following', properties.login);
+
+  writeJsonFile('followers', followers);
+  writeJsonFile('following', following);
+
+  //verifyUsersFollowing();
+})();

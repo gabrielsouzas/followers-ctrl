a3c25582f9d7f2401a5ab48aaec12d6b4280b254
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2024-02-05 10:34:34 -0300

    add: Function get all itens

--- github-api-fetch/fetchFollowers.js
+++ github-api-fetch/fetchFollowers.js
@@ -1,39 +1,77 @@
-const Octokit = require("@octokit/core").Octokit
+const Octokit = require('@octokit/core').Octokit;
 const fs = require('fs');
 const properties = require('./properties.json');
 
 const octokit = new Octokit();
-// A FAZER: aumentar paginas recebidas
-const fetchFollowers = async (value, user, filter = '', per_page = '100') => {
-  /*const response = await octokit.request(`GET /users/{username}/${value}?per_page=100?page=1`, {*/
-  const response = await octokit.request(`GET /users/{username}/${value}?per_page=${per_page}`, {
-    username: user
-  })
+
+/* const fetchFollowers = async (value, user, filter = '', per_page = '100') => {
+  const response = await octokit.request(
+    `GET /users/{username}/${value}?per_page=${per_page}&page=${page}`,
+    {
+      username: user,
+    }
+  );
 
   var data = response.data;
 
   if (filter != '') {
-    data = data.map( dt => ({
-      filter: dt[`${filter}`]
-    }))
+    data = data.map((dt) => ({
+      filter: dt[`${filter}`],
+    }));
   }
-  
-  /*
-    data = data.map( ({login}) => ({
-      login
-    }))
-  
-    data.forEach(element => {
-      console.log(element)
-    });*/
+
+  return data;
+}; */
+
+const fetchFollowers = async (
+  value,
+  user,
+  filter = '',
+  per_page = '100',
+  max_pages = 4
+) => {
+  let response = {};
+  for (let i = 1; i <= max_pages; i++) {
+    let responsePage = await octokit.request(
+      `GET /users/{username}/${value}?per_page=${per_page}&page=${i}`,
+      {
+        username: user,
+      }
+    );
+
+    if (responsePage.length === 0) {
+      return;
+    }
+
+    response = {
+      ...response,
+      responsePage,
+    };
+
+    await aguardarSegundos(2);
+  }
+
+  var data = response.data;
+
+  if (filter != '') {
+    data = data.map((dt) => ({
+      filter: dt[`${filter}`],
+    }));
+  }
 
   return data;
 };
 
+function awaitSeconds(seconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
+
 const fetchDetails = async (user) => {
   const response = await octokit.request(`GET /users/${user}`, {
     //username: user
-  })
+  });
 
   var data = response.data;
 
@@ -42,7 +80,7 @@ const fetchDetails = async (user) => {
       filter: dt[`${filter}`]
     }))
   }*/
-  
+
   /*
     data = data.map( ({login}) => ({
       login
@@ -53,27 +91,62 @@ const fetchDetails = async (user) => {
 };
 
 // escrever os dados em um arquivo local (json)
-function writeJsonFile(fileName, fileData){
-  fs.writeFile(`${fileName}.json`, JSON.stringify(fileData, null, 2), err => {
-    if(err) throw new Error('Erro: '+ err)
+function writeJsonFile(fileName, fileData) {
+  fs.writeFile(`${fileName}.json`, JSON.stringify(fileData, null, 2), (err) => {
+    if (err) throw new Error('Erro: ' + err);
 
-    console.log(`${fileName} fetched successfully!`)
-  })
-}
-
-async function verifyUsersFollowing(){
-  var arrayRobots = [];
-  let followingLogins = await fetchFollowers('following', properties.login);
-  followingLogins.forEach(async el => {
-    let userFollowing = await fetchFollowers('following', el.login, 'login', '5');
-    console.log(el.login)
-    console.log(userFollowing.length)
+    console.log(`${fileName} fetched successfully!`);
   });
 }
 
+async function verifyUsersFollowing() {
+  var arrayRobots = [];
+  let followingLogins = await fetchFollowers('following', properties.login);
+  followingLogins.forEach(async (el) => {
+    let userFollowing = await fetchFollowers(
+      'following',
+      el.login,
+      'login',
+      '5'
+    );
+    console.log(el.login);
+    console.log(userFollowing.length);
+  });
+}
+
+async function fetchAll(res, user, per_page = '100') {
+  let allItens = [];
+  let page = 1;
+
+  try {
+    while (true) {
+      const url = `GET /users/{username}/${res}?per_page=${per_page}&page=${page}`;
+      const response = await octokit.request(url, {
+        username: user,
+      });
+      var data = response.data;
+
+      if (data.length === 0) {
+        break;
+      }
+
+      allItens = allItens.concat(data);
+      page++;
+    }
+
+    return allItens;
+  } catch (erro) {
+    console.error('Erro ao obter dados:', erro);
+    throw erro;
+  }
+}
+
 (async () => {
-  const followers = await fetchFollowers('followers', properties.login);
-  const following = await fetchFollowers('following', properties.login);
+  const following = await fetchAll('following', properties.login);
+  const followers = await fetchAll('followers', properties.login);
+
+  //const followers = await fetchFollowers('followers', properties.login);
+  //const following = await fetchFollowers('following', properties.login);
 
   writeJsonFile('followers', followers);
   writeJsonFile('following', following);
